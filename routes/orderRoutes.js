8a90d75b903020c2ce8677497bf8899a1e345478
Rajpateriya <pateriyaraj52@gmail.com>
2024-06-14 15:00:51 +0530

    orders routes controllers & model completed

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'
 const router = express.Router()
 
-import { addOrderItems,getOrderById,updateOrdeToPaid ,getOrders} from '../controllers/orderController.js'
+import { addOrderItems,getOrderById,updateOrdeToPaid ,getOrders,getMyOrders,updateOrdeToDelivered} from '../controllers/orderController.js'
 
 import {protect , admin} from '../middlewares/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems).get(protect,admin ,getOrders)
+router.route('/myorders').get(protect , getMyOrders)
+
+router.route('/:id').get(protect ,getOrderById)
 router.route('/:id/pay').put(protect , updateOrdeToPaid)
+router.route('/:id/deliver').put(protect,admin, updateOrdeToDelivered)
 
 export default router
